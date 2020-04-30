1aa37b96675e718ef17da3be9ec0b4d9b982e4c6
Abhimanyu Soni <abhimanyushady@gmail.com>
2020-04-30 11:31:31 +0530

    Updates

---
+++ precache-manifest.c816ff92fd1865c21eaf0300a29e3660.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93ff6d201dcd3acb698ea912de66bbb7",
+    "url": "/my-blog/index.html"
+  },
+  {
+    "revision": "edf37bc996af241d1e03",
+    "url": "/my-blog/static/css/main.be322b42.chunk.css"
+  },
+  {
+    "revision": "3e1fff4768d5d9182d36",
+    "url": "/my-blog/static/js/2.c4c48145.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-blog/static/js/2.c4c48145.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "edf37bc996af241d1e03",
+    "url": "/my-blog/static/js/main.a3f9270f.chunk.js"
+  },
+  {
+    "revision": "033396f38d5c55a0d099",
+    "url": "/my-blog/static/js/runtime-main.af636974.js"
+  }
+]);
